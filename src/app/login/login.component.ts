484803448ab7171879d8c795597f1ed4802c1cfc
afaf99 <125907668+afaf99@users.noreply.github.com>
2023-08-01 02:06:49 +0300

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -12,7 +12,7 @@ export class LoginComponent implements OnInit{
 
   username ='Afaf'
   password =''
-  errorMessage ='Invalid Credentials'
+  errorMessage ='Invalid Credentialsff'
   invalidLogin= false
 
   //Router
@@ -28,49 +28,31 @@ export class LoginComponent implements OnInit{
   handleLogin(){
     // console.log(this.username);
     // if(this.username === "Afaf" && this.password=== 'dummy'){
-        if(this.hardcodedAuthenticationService.authenticate(this.username, this.password)){
+      
 
       this.router.navigate(['welcome', this.username])
 
       this.invalidLogin = false
-    }else{
-      this.invalidLogin = true
-    }
+    
   }
 
   handleBasicAuthLogin(){
     // console.log(this.username);
     // if(this.username === "Afaf" && this.password=== 'dummy'){
-        this.basicAuthenticationService.executeAuthenticationService(this.username, this.password)
-        .subscribe(
-          data => {
-            console.log(data)
+        
+         
+        
             this.router.navigate(['welcome', this.username])
             this.invalidLogin = false
-          },
-          error => {
-            console.log(error)
-            this.invalidLogin = true
-          }
-        )  
+          
+        
     }
 
     handleJWTBasicAuthLogin(){
-      // console.log(this.username);
-      // if(this.username === "Afaf" && this.password=== 'dummy'){
-          this.basicAuthenticationService.executeJWTAuthenticationService(this.username, this.password)
-          .subscribe(
-            data => {
-              console.log(data)
-              this.router.navigate(['welcome', this.username])
-              this.invalidLogin = false
-            },
-            error => {
-              console.log(error)
-              this.invalidLogin = true
-            }
-          )  
+
+
+      this.router.navigate(['welcome', this.username])
+      this.invalidLogin = false
       }
   }
   
-
